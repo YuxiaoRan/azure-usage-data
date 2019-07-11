569c090d8695b560db9262e8e24a91bbf50f9fec
Sean Ran <sean.ran@catapultsystems.com>
2019-07-11 16:58:33 -0500

    script can now auto-login and work for all subscriptions

--- azurescript2_2.ps1
+++ azurescript2_2.ps1
@@ -12,55 +12,59 @@ $reportedEndTime = "2019-06-26"
 # Auto-Login:
 Import-AzureRmContext -Path "c:\Users\sran\Documents\Azure_PowerShell_Scripts\credentials\sran_profile.json"
 
-# Select an Azure Subscription for which to report usage data
-$subscriptionId = 
-    (Get-AzureRmSubscription |
-     Out-GridView `
-        -Title "Select an Azure Subscription ..." `
-        -PassThru).SubscriptionId
-Select-AzureRmSubscription -SubscriptionId $subscriptionId
+# Get all Azure subscriptions
+$subscriptions = Get-AzureRmSubscription
 
-# Set path to exported CSV file
-$filename = "c:\Users\sran\Documents\Azure_PowerShell_Scripts\usageData-${subscriptionId}-${reportedStartTime}-${reportedEndTime}.csv"
+# Use for-each to loop over all subscriptions
+ForEach($sub in $subscriptions){
+    $subId = $sub.subscriptionId
+    Select-AzureRmSubscription -SubscriptionId $subId
+    Write-Host
+    Write-Host "Working on " $subId
+    Write-Host
+    
+    # Set path to exported CSV file
+    $filename = "c:\Users\sran\Documents\Azure_PowerShell_Scripts\usageData-${subId}-${reportedStartTime}-${reportedEndTime}.csv"
 
-# Set usage parameters
-$granularity = "Daily" # Can be Hourly or Daily
-$showDetails = $true
+    # Set usage parameters
+    $granularity = "Daily" # Can be Hourly or Daily
+    $showDetails = $true
 
-# Export Usage to CSV
-$appendFile = $false
-$continuationToken = $null
-Do { 
-    $usageData = Get-UsageAggregates `
-        -ReportedStartTime $reportedStartTime `
-        -ReportedEndTime $reportedEndTime `
-        -AggregationGranularity $granularity `
-        -ShowDetails:$showDetails `
-        -ContinuationToken $continuationToken
-    $usageData.UsageAggregations.Properties | 
-        Select-Object `
-            UsageStartTime, `
-            UsageEndTime, `
-            @{n='SubscriptionId';e={$subscriptionId}}, `
-            MeterCategory, `
-            MeterId, `
-            MeterName, `
-            MeterSubCategory, `
-            MeterRegion, `
-            Unit, `
-            Quantity, `
-            @{n='Project';e={$_.InfoFields.Project}}, `
-            InstanceData | 
-        Export-Csv `
-            -Append:$appendFile `
-            -NoTypeInformation:$true `
-            -Path $filename
-    if ($usageData.NextLink) {
-        $continuationToken = `
-            [System.Web.HttpUtility]::`
-            UrlDecode($usageData.NextLink.Split("=")[-1])
-    } else {
-        $continuationToken = ""
-    }
-    $appendFile = $true
-} until (!$continuationToken)
+    # Export Usage to CSV
+    $appendFile = $false
+    $continuationToken = $null
+    Do { 
+        $usageData = Get-UsageAggregates `
+            -ReportedStartTime $reportedStartTime `
+            -ReportedEndTime $reportedEndTime `
+            -AggregationGranularity $granularity `
+            -ShowDetails:$showDetails `
+            -ContinuationToken $continuationToken
+        $usageData.UsageAggregations.Properties | 
+            Select-Object `
+                UsageStartTime, `
+                UsageEndTime, `
+                @{n='SubscriptionId';e={$subscriptionId}}, `
+                MeterCategory, `
+                MeterId, `
+                MeterName, `
+                MeterSubCategory, `
+                MeterRegion, `
+                Unit, `
+                Quantity, `
+                @{n='Project';e={$_.InfoFields.Project}}, `
+                InstanceData | 
+            Export-Csv `
+                -Append:$appendFile `
+                -NoTypeInformation:$true `
+                -Path $filename
+        if ($usageData.NextLink) {
+            $continuationToken = `
+                [System.Web.HttpUtility]::`
+                UrlDecode($usageData.NextLink.Split("=")[-1])
+        } else {
+            $continuationToken = ""
+        }
+        $appendFile = $true
+    } until (!$continuationToken)
+}
